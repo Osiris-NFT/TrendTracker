ff66575370fd77507ce32b252cb9b5e6d9497399
Yoann-Renard <yoann.rnrd@gmail.com>
2022-03-28 20:05:06 +0200

    init API

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, Response, status
+import os
+import json
+
+from src.classes.settings import *
+
+
+app = FastAPI()
+
+@app.get("/get_best_launch_list")
+async def get_best_lunched_publications_list(response: Response): # TODO TEST
+    if os.path.exists(os.path.join(VOLUME_PATH, "best_launch_result.json")):
+        with open(os.path.join(VOLUME_PATH, "best_launch_result.json"), 'r') as f:
+            result = json.loads(f)
+        response.status_code = status.HTTP_200_OK
+        return result
+    else:
+        response.status_code = status.HTTP_204_NO_CONTENT
+        return {
+            "message": "No content"
+        }
--- src/script.py
+++ src/script.py
@@ -1,9 +1,5 @@
 import requests
-from fastapi import FastAPI
 from classes.best_launch import BestLaunch
-
-app = FastAPI()
-
 if __name__ == '__main__':
     best_lauch = BestLaunch()
     # need to start threads if multiple scripts
