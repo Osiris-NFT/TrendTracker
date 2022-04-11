0db988f369ce7543d5c80fd11cd45fec4e8af5e2
Yoann-Renard <yoann.rnrd@gmail.com>
2022-04-11 19:25:56 +0200

    start fix

--- src/classes/settings.py
+++ src/classes/settings.py
@@ -8,8 +8,8 @@ PUBLICATION_SVC_URL = "http://0.0.0.0:8000/"
 VOLUME_PATH = os.environ["VOLUME_PATH"]
 PUBLICATION_SVC_URL = os.environ["PUBLICATION_SVC_URL"]
 
-PUBLICATION_SVC_GET_RECENT_ENDPOINT = "trend_tracker_get_recent_publications"
-PUBLICATION_SVC_GET_MANY_ENDPOINT = "trend_tracker_get_many_publications"
+PUBLICATION_SVC_GET_RECENT_ENDPOINT = "/trend_tracker_get_recent_publications"
+PUBLICATION_SVC_GET_MANY_ENDPOINT = "/trend_tracker_get_many_publications"
 
 #### BEST LAUNCH ####
 BL_UPDATE_OCCURENCE = 1 # HOUR(S)
